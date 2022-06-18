02c783a65d3a77ef8315b02f6b6112655fc4bfd3
Aadv1k <aadv1k@outlook.com>
2022-06-18 19:33:56 +0530

    format the code via prettier

--- leetcode/longestCommonPrefix.js
+++ leetcode/longestCommonPrefix.js
@@ -2,21 +2,21 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) { 
+var longestCommonPrefix = function (strs) {
   let curStr = "";
 
-  if (strs[0].length === 0) return '';
+  if (strs[0].length === 0) return "";
 
   for (let i = 0; i < 200; i++) {
-    let stack = []
-    stack.push(...strs.map(e => e.slice(i, i+1)))
-    
+    let stack = [];
+    stack.push(...strs.map((e) => e.slice(i, i + 1)));
+
     if (stack.every((a) => a === stack[0]) && stack[0]) {
-      curStr = curStr.concat(stack.pop())
+      curStr = curStr.concat(stack.pop());
     } else {
-      return curStr
+      return curStr;
     }
-  } 
+  }
 };
 
 module.exports = longestCommonPrefix;
--- leetcode/romanToInteger.js
+++ leetcode/romanToInteger.js
@@ -5,8 +5,8 @@
 var romanToInt = function (s) {
   const target = s.trim().split("");
 
-  const dict = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000, };
-  const special = { I: ["V", "X"], X: ["L", "C"], C: ["D", "M"]};
+  const dict = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  const special = { I: ["V", "X"], X: ["L", "C"], C: ["D", "M"] };
   const stack = [];
 
   for (let i = 0; i < target.length; i++) {
@@ -15,7 +15,7 @@ var romanToInt = function (s) {
       special[target[i - 1]].includes(target[i])
     ) {
       stack.pop();
-      stack.push(dict[target[i]] - dict[target[i - 1]])
+      stack.push(dict[target[i]] - dict[target[i - 1]]);
     } else {
       stack.push(dict[target[i]]);
     }
